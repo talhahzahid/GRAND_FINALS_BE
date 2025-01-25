e8e266ae8abdf796220374e8c9a68de6ea65cfba
talhahzahid <talhazahid415@gmail.com>
2025-01-26 01:20:10 +0500

    Fifty person done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import connectdb from "./src/db/index.js";
 import authRouter from "./src/routes/auth.routes.js"
 import cookieParser from "cookie-parser";
 import clientRoutes from "./src/routes/client.routes.js"
+import loanRoutes from "./src/routes/loan.routes.js"
 const app = express();
 
 const corsOption = {
@@ -17,6 +18,7 @@ app.use(cors(corsOption));
 app.use(cookieParser())
 app.use("/api/v1", authRouter)
 app.use("/api/v2", clientRoutes)
+app.use("/api/v3", loanRoutes)
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
